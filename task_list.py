3afac49eb60814c7f1f28d21b4ef8bfc8db08402
Benjamin Smith <bsmith@cabbage.org.uk>
2022-10-19 16:09:19 +0100

    First solution

--- task_list.py
+++ task_list.py
@@ -6,33 +6,52 @@ tasks = [
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
 
+def print_task(task):
+    completed_msg = "done" if task['completed'] else "UNCOMPLETED"
+    print(f"* {task['description']:25s}\t{completed_msg:11s}\t[{task['time_taken']:2d}]")
+
 def print_uncompleted_tasks():
-    pass
-    # raise NotImplementedError()
+    for task in tasks:
+        if task['completed'] == False:
+            print_task(task)
 
 def print_completed_tasks():
-    pass
-    # raise NotImplementedError()
+    for task in tasks:
+        if task['completed']:
+            print_task(task)
 
 def print_all_tasks():
-    pass
-    # raise NotImplementedError()
+    for task in tasks:
+        print_task(task)
 
 def print_tasks_minimum_time_taken(minimum_time_taken):
-    pass
-    # raise NotImplementedError()
+    for task in tasks:
+        if task['time_taken'] >= minimum_time_taken:
+            print_task(task)
+
+def search_list(list, predicate):
+    for item in list:
+        if predicate(item):
+            return item
+    return None
+
+def find_task_by_description(description):
+    return search_list(tasks, lambda task: task['description'] == description)
 
 def print_task_by_description(description):
-    pass
-    # raise NotImplementedError()
+    found_task = find_task_by_description(description)
+    print_task(found_task)
 
 def mark_task_as_complete(description):
-    pass
-    # raise NotImplementedError()
+    found_task = find_task_by_description(description)
+    found_task['completed'] = True
 
 def add_task(description, completed, time_taken):
-    pass
-    # raise NotImplementedError()
+    tasks.append({
+        'description': description,
+        'completed': completed,
+        'time_taken': time_taken,
+    })
 
 print("=== UNCOMPLETED TASKS ===")
 print_uncompleted_tasks()
